b59274f2a59eef7a658fa3f1a83a7ddd53f478fb
Jotaro <frankiss.pc@gmail.com>
2020-05-07 20:06:39 +0200

    Modal login y registro

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,6 +8,10 @@ use Symfony\Component\Routing\Annotation\Route;
 //Importamos las entidades
 use App\Entity\CategoryTb;
 
+//Importamos los tipos de campo de formularios que necesitamos
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
 class HomeController extends AbstractController
 {
     public function index(){
@@ -39,10 +43,40 @@ class HomeController extends AbstractController
           }
         }
 
-        return $this->render('home/navbar.html.twig', [
+        return $this->render('_includes/blocks/navbar.html.twig', [
             'categories' => $categories,
             'subcategories' => $subcategories,
             'currentPath' => $currentPath
         ]);
+    }
+
+    public function loginPopup(){
+      $form = $this->createFormBuilder()
+						->add('nick', TextType::class,['label' => 'Nombre de usuario:'])
+            ->add('password', PasswordType::class,['label' => 'Nombre de usuario:'])
+          				->getForm();
+      $icon = "<i class='fas fa-user'></i>";
+
+      return $this->render('_includes/blocks/loginPopup.html.twig', [
+        'controller_name' => 'HomeController',
+        'modalTitle' => "Iniciar Sesión",
+        'form' => $form->createView(),
+        'icon' => $icon
+      ]);
+    }
+
+    public function registerPopup(){
+      $form = $this->createFormBuilder()
+            ->add('nick', TextType::class,['label' => 'Nombre de usuario:'])
+            ->add('password', PasswordType::class,['label' => 'Nombre de usuario:'])
+                  ->getForm();
+      $icon = '<i class="fas fa-address-book"></i>';
+
+      return $this->render('_includes/blocks/registerPopup.html.twig', [
+        'controller_name' => 'HomeController',
+        'modalTitle' => "Registrarte",
+        'form' => $form->createView(),
+        'icon' => $icon
+      ]);
     }
 }
